17dc1957ec8c0988b291b61174ebd93967e446d8
Zedor1S <115577754+Zedor1S@users.noreply.github.com>
2024-03-12 22:17:51 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ python_script="test.py"
 # Перевірка чи файл існує
 if [ -f "$python_script" ]; then
     echo "Запускаю Python скрипт $python_script"
-    python "$python_script" # Запускаємо Python скрипт
+    python3 "$python_script" # Запускаємо Python скрипт
 else
     echo "Помилка: файл $python_script не знайдено або не може бути запущений."
     exit 1
